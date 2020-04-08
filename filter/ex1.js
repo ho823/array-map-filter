75eb63cccc929778708fefb8da08170adf6465a5
Ameline <amelyne.ho@hotmail.fr>
2020-04-08 17:34:36 +0200

    Exercice 1 filter terminé

--- filter/ex1.js
+++ filter/ex1.js
@@ -13,8 +13,12 @@ Expected OUTPUT for this sample:
 
 */
 
-function getPositiveNumbers(numbers) {
-}
+  function getPositiveNumbers(arrayNumbers){
+    return arrayNumbers.filter((numbers) => numbers >= 0);
+  }
+  
+  console.log(getPositiveNumbers([7, -4, 2, 0, -10, 3, 11]));
+
 
 // If you need, you can always visualize the result with a console.log.
 // In that case, uncomment the line below (you can also add your own
